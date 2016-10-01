f06d76c28a23b329990e7c06d1f051fc71f9f6d6
jaronoff97 <jacobaronoff45@gmail.com>
2016-10-01 12:10:47 -0400

    initial commit

--- AlwaysBlue/ViewController.swift
+++ AlwaysBlue/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var recordButton: UIButton!
+    lazy var motionManager = CMMotionManager()
+    let queue = OperationQueue()
+    var accel_data:[String] = [String]()
+    var gyro_data:[String] = [String]()
+    var recording = false
+    
+    @IBAction func start_stop(_ sender: AnyObject) {
+        recording = !recording
+        print("------------")
+        print(accel_data)
+        print("------------")
+        print(gyro_data)
+        print("------------")
+        accel_data = []
+        gyro_data = []
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if motionManager.isAccelerometerAvailable{
+            motionManager.startAccelerometerUpdates(to: queue, withHandler:
+                {data, error in
+                    
+                    guard let data = data else{
+                        return
+                    }
+                    if self.recording {
+                        self.accel_data.append("\(data.acceleration.x),\(data.acceleration.y),\(data.acceleration.z)")
+                    }
+                }
+            )
+        } else {
+            print("Accelerometer is not available")
+        }
+        if motionManager.isGyroAvailable {
+            motionManager.deviceMotionUpdateInterval = 0.1;
+            motionManager.startDeviceMotionUpdates()
+            
+            motionManager.gyroUpdateInterval = 0.1
+            motionManager.startGyroUpdates(to: queue, withHandler: { (data, error) in
+                guard let data = data else {
+                    return
+                }
+                if self.recording {
+                    self.gyro_data.append("\(data.rotationRate.x),\(data.rotationRate.y),\(data.rotationRate.z)")
+                }
+            })
+        } else {
+            // alert message
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
